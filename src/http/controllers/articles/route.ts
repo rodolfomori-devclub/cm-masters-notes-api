aefc9882f6303673146bf02f4a594550a767b108
Agustinho Neto <hello@apneto.dev>
2024-08-28 21:57:43 -0300

    feat(articles): adds list and remove routes

--- src/http/controllers/articles/route.ts
+++ src/http/controllers/articles/route.ts
@@ -1,6 +1,10 @@
 import type { FastifyInstance } from 'fastify';
 import { create } from './create';
+import { list } from './list';
+import { remove } from './remove';
 
 export async function articlesRoutes(app: FastifyInstance) {
 	app.post('/articles', create);
+	app.get('/articles', list);
+	app.delete('/articles/:id', remove);
 }
